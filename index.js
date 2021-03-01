46299da9d62edc1fa9f42dc52d809be89a15f78d
inwerpsel <pieter.vincent1988@gmail.com>
2021-03-01 10:18:35 +0100

    Allow generating name from selector with _--
    * By removing all non-alpha, non-dash characters with 2 dashes.
    * Simplify regex.
    * Again specific notation `_--` is one of the few things that can work
    for this. First wanted to use just `--`, but that is considered invalid
    syntax in the current setup, whereas the former is not.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const postcss = require("postcss");
 
+const generatePrefix = selector => {
+    // Replace
+    // 1. the shorthand
+    // 2. consecutive non word or dash chars with --
+    // 3&4. remove all dashes at start and end, so we can put exactly 2 on both ends.
+    return `--${selector.replace(/_--/, '').replace(/[^\w-]+/g, '--').replace(/^-*/, '').replace(/-*$/, '')}--`;
+}
+
+const DASH_DASH_REGEX = /^(.+ )?_?--(\w+(-\w+)*--)*(:(hover|focus|active|disabled|visited))?$/;
+
 const replaceShorthandSelectors = (css, result) => {
     let prevRule = null;
     css.walkRules(rule => {
         const rulesWithDashDash = rule.selectors.filter(
-            selector => /^(.+ )?--[a-z]\w*(--?[a-z0-9]+)*--(:(hover|focus|active|disabled|visited))?$/.test(selector)
+            selector => DASH_DASH_REGEX.test(selector)
         );
 
         // Nothing to do here, go to next rule.
@@ -23,13 +33,19 @@ const replaceShorthandSelectors = (css, result) => {
         parts.reverse();
         const lastPart = parts[0];
 
-        const prefix = `${lastPart.replace(/:/, '').replace(/--$/, '')}--`;
-        const withoutState = lastPart.replace(/:(hover|focus|active|disabled|visited)/, '');
+        const prefix = `${lastPart.replace(/[:_]/g, '').replace(/--$/, '')}--`;
+        const withoutPseudo = lastPart.replace(/:(hover|focus|active|disabled|visited)/g, '');
+
+        const isPrefixGenerate = '_--' === withoutPseudo;
+
+        if ( isPrefixGenerate && rule.selectors.length > 1) {
+            throw new Error('Cannot generate prefix when using multiple selectors.');
+        }
 
         const newDecls = [];
 
         rule.walkDecls(decl=> {
-            const varName = `${prefix}${decl.prop}`;
+            const varName = `${isPrefixGenerate ? generatePrefix(rule.selectors[0]) : prefix}${decl.prop}`;
             const newDecl = postcss.decl({
                 prop: decl.prop,
                 value: `var(${varName}, ${decl.value})`,
@@ -38,7 +54,8 @@ const replaceShorthandSelectors = (css, result) => {
             decl.remove();
         });
 
-        const expectedTargetSelectors = rule.selectors.map(selector => selector.replace(` ${withoutState}`, ''));
+        // Preserve the pseudo class.
+        const expectedTargetSelectors = rule.selectors.map(selector => selector.replace(` ${withoutPseudo}`, ''));
         const expectedTargetSelector = expectedTargetSelectors.join();
 
         if (prevRule && prevRule.parent === rule.parent && prevRule.selectors.join() === expectedTargetSelector) {
